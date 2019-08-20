636dc29675b6ac318c1930a1103c848cfecf0347
studiousryan <studiousryan@gmail.com>
2019-08-20 15:14:23 +0800

    项目初始化

---
+++ JianChi/JianChi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JianChi
+//
+//  Created by Ryan on 2019/8/20.
+//  Copyright © 2019 Daoxiang Deng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
